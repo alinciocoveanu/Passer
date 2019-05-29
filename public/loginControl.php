2e5609d8a1799ff60ef4386a86d958070e696c68
alinciocoveanu <33698707+alinciocoveanu@users.noreply.github.com>
2019-05-29 15:34:00 +0300

    remove include + more htaccess + html response codes

--- public/loginControl.php
+++ public/loginControl.php
@@ -5,10 +5,17 @@ define('ROOT8', dirname(dirname(__FILE__)));
 require_once(ROOT8 . DS8 . 'application' . DS8 . 'models' . DS8 . 'UserModel.php');
 require_once(ROOT8 . DS8 . 'application' . DS8 . 'controllers' . DS8 . 'UsersController.php');
 
+function badRequest() {
+    http_response_code(400);
+    die('400 Bad Request');
+}
+
+
 $op = '';
 
 if(isset($_REQUEST['op']))
     $op = $_REQUEST['op'];
+else badRequest();
 
 switch($op) {
     case 'login':
@@ -17,10 +24,14 @@ switch($op) {
         if(isset($_POST['username'], $_POST['password'])) {
             if($userController->logUser($_POST['username'], $_POST['password'])) {
                 header("Location:/Passer/application/views/account.php");
-            } else
+            } else {
                 header("Location:/Passer/application/views/index.php?err=1");
-        } else
-        header("Location:/Passer/application/views/index.php?err=1");
+                badRequest();
+            }
+        } else {
+            header("Location:/Passer/application/views/index.php?err=1");
+            badRequest();
+        }
         break;
 
     case 'logout':
@@ -39,10 +50,19 @@ switch($op) {
 
             if($userController->createUser($user, $password)) {
                 header("Location:/Passer/application/views/account.php");
-            } else
+            } else {
                 header("Location:/Passer/application/views/createAccount.php?err=1");
-        } else
+                badRequest();
+            }
+        } else {
             header("Location:/Passer/application/views/createAccount.php?err=1");
+            badRequest();
+        }
+        break;
+
+    default:
+        http_response_code(404);
+        die('404 Not Found');
         break;
 }
 ?>
